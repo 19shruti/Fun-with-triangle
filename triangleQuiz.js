31a65cee7a74649dead08d58f7380caa04450d81
SHRUTI RAJ <69633187+19shruti@users.noreply.github.com>
2021-08-08 19:43:39 +0530

    finall

--- triangleQuiz.js
+++ triangleQuiz.js
@@ -3,33 +3,38 @@
 const  quizForm=document.forms[0];
 const que=document.querySelectorAll("#que");
 const scorebox=document.querySelector(".scorehere");
-const submit=document.getElementById("submit");
+const submit=document.querySelector("#submit");
+const ans=document.querySelectorAll("#ans");
+
 const correctAns=["option1","option3","option2","option1","option3"];
 
 let score=0; 
+ console.log("entry");
 
-quizForm.addEventListener('submit',(e)=>
-{  
+quizForm.addEventListener('submit',(e)=>{  
   
     e.preventDefault();
   
     const data= new FormData(quizForm);
+
     let index=0;
   
 
 
     for(let entry of data)
+   
     {
         if(entry[1] == correctAns[index]){
-            que[index].style.background="lightgreen";
+            que[index].style.background="green";
             score++
         }
         else{
-            que[index].style.background="pink";
+            que[index].style.background="red";
+            ans.innerHTML="the correct ans is :: "+correctAns[index];
         }
         index++;
     }
-    scorebox.innerText = score;
+    scorebox.innerHTML = "Your score is "+score;
     submit.style.display="none";
 });  
 
